a3805baad47ac8d76034ac5348424ed1b99d752d
Simon <simon@kovtyk.com>
2025-11-27 08:52:44 +0100

    docs: add types

--- src/types/anguler-rules.type.ts
+++ src/types/anguler-rules.type.ts
@@ -5,6 +5,13 @@
  * @author Simon Kovtyk
  */
 type Options = {
+  /**
+   * Angular selector prefix for components and directives
+   * @remarks
+   * If you want to enforce, that every component selector should start with `app-`, set it to `app`.
+   *
+   * @since 1.0.0
+   */
   selectorPrefix: string
 };
 
